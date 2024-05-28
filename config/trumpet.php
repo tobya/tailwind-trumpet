a025fa02a317f604fce2b0a61203d742ca3348fd
tobya <tobya@users.noreply.github.com>
2024-05-28 15:53:03 +0000

    Fix styling

--- config/trumpet.php
+++ config/trumpet.php
@@ -1,11 +1,10 @@
 <?php
 
-
 return [
     'register' => [
         // add classes here that will return a list of classes that need to be exposed.
         // classes must have a function trumpetTailwindClasses() that returns a String or array of Strings
 
         //\Tobya\TailwindTrumpet\TailwindTrumpet::class,
-    ]
+    ],
 ];
